3e29ad9b3b2a60cfca721c1ab9584854b05270a5
Cris <csimonvalero@gmail.com>
2019-08-26 11:22:35 +0200

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MenuComponentComponent } from './shared/menu-component/menu-component.component';
+import { FormTableComponent } from './pages/form/form-table/form-table.component';
+import { FormEditComponent } from './pages/form/form-edit/form-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponentComponent,
+    FormTableComponent,
+    FormEditComponent
   ],
   imports: [
     BrowserModule,
